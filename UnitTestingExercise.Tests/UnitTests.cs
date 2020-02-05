aeac96b7c640bebe88adb5b515b58fae5593f426
William Dodson <wcdodson4@gmail.com>
2020-02-05 13:42:43 -0600

    almost done

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,56 +1,65 @@
 using System;
 using Xunit;
+using UnitTestingExercise;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 3, 6)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var added = new UnitTestMethods();
 
             //Act
+            var actual = added.Addition(num1, num2, num3);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(10, 6, 4)]
+        public void Subtract(int minuend, int subtrahend, int expected)
         {
             //Arrange
+            var subtracted = new UnitTestMethods();
 
             //Act
+            var actual = subtracted.Subtraction(minuend, subtrahend);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(4, 8, 32)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var multiplied = new UnitTestMethods();
 
             //Act
+            var actual = multiplied.Multiplication(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(42, 4, 10.5)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var divided = new UnitTestMethods();
 
             //Act
+            var actual = divided.Division(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -4,20 +4,36 @@ namespace UnitTestingExercise
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Righ-click on Dependencies in your .Tests project and set your reference
+        public UnitTestMethods()
+        {
 
-
+        }
 
         // Create an Add method that passes 3 integers
+        public int Addition(int x, int y, int z)
+        {
+            return x + y + z;
+        }
 
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
 
         // Create a Multiply method that passes 2 integers
+        public int Multiplication(int p, int q)
+        {
+            return p * q;
+        }
 
         // Create a Divide method that passes 2 integers
-
-
+        public double Division(int i, int j)
+        {
+            return i / j;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
